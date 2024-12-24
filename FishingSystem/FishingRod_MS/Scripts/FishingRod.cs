5f3074939cfae7b9d1e190e22b2799eebdfa0a35
emirralkn60 <emirralkn@gmail.com>
2024-12-24 19:45:04 +0300

    FishingSystem
    FishingSystem

---
+++ FishingSystem/FishingRod_MS/Scripts/FishingRod.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FishingRod : MonoBehaviour
+{
+    public bool isEquipped;
+    public bool isFishingAvailable;
+
+    public bool isCasted;
+    public bool isPulling;
+
+    Animator animator;
+    public GameObject baitPrefab;
+    public GameObject endof_of_rope;  // --- > IF USING ROPE
+    public GameObject start_of_rope;   // --- > IF USING ROPE   
+    public GameObject start_of_rod;    // --- > IF USING ROPE   
+
+    Transform baitPosition;
+    GameObject baitReference;
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        isEquipped = true;
+    }
+
+    private void OnEnable()
+    {
+        FishingSystem.OnFishingEnd += HandleFishingEnd;
+    }
+    private void OnDestroy()
+    {
+        FishingSystem.OnFishingEnd-= HandleFishingEnd;
+    }
+    public void HandleFishingEnd()
+    {
+        Destroy(baitReference);
+    }
+
+    void Update()
+    {
+        if (isEquipped)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+
+                if (hit.collider.CompareTag("FishingArea"))
+                {
+                    isFishingAvailable = true;
+
+                    if (Input.GetMouseButtonDown(0) && !isCasted && !isPulling)
+                    {
+                         waterSource Source=hit.collider.gameObject.GetComponent<FishingArea>().waterSource;
+                        StartCoroutine(CastRod(hit.point, Source));
+                    }
+                }
+                else
+                {
+                    isFishingAvailable = false;
+                }
+
+            }
+            else
+            {
+                isFishingAvailable = false;
+            }
+        }
+
+        // --- > IF USING ROPE < --- //
+        if (isCasted || isPulling)  
+        {
+            if (start_of_rope  != null && start_of_rod != null && endof_of_rope != null)
+            {
+                start_of_rope.transform.position = start_of_rod.transform.position;
+
+                if (baitPosition != null)
+                {
+                    endof_of_rope.transform.position = baitPosition.position;
+                }
+            }
+            else
+            {
+                Debug.Log("MISSING ROPE REFERENCES");
+            }
+        } 
+     
+        if (isCasted && Input.GetMouseButtonDown(1) && FishingSystem.instance.isThereABite)
+        {
+            PullRod();
+        }
+    }
+
+
+    IEnumerator CastRod(Vector3 targetPosition, waterSource source)
+    {
+        isCasted = true;
+        animator.SetTrigger("Cast");
+        
+        // Create a delay between the animation and when the bait appears in the water
+        yield return new WaitForSeconds(1f);
+   
+        GameObject instantiatedBait = Instantiate(baitPrefab);
+        instantiatedBait.transform.position = targetPosition;
+
+        baitPosition = instantiatedBait.transform;
+        baitReference = instantiatedBait;
+        // ---- > Start Fish Bite Logic
+        FishingSystem.instance.StartFishing(source);
+    }
+
+    private void PullRod()
+    {
+        animator.SetTrigger("Pull");
+        isCasted = false;
+        isPulling = true;
+
+        // ---- > Start Minigame Logic
+        FishingSystem.instance.SetHasPulled();
+    }
+}
